b28f5a2434c9f8bef8852550aeb17bf9de35e556
Antony Cartwright <antony.cartwright@awaze.com>
2024-05-07 00:18:43 +0200

    WIP: Bootstrap before deployment

--- scripts/export-source.sh
+++ scripts/export-source.sh
@@ -3,8 +3,20 @@
 # Usage: ./scripts/export-source.sh
 mkdir -p './exports/'
 find "." -type f -not -path '*/programs/*' -not -path '*/target/*' -not -path '*/.terragrunt-cache/*' -not -path '*/results/*' -not -path '*/actions/*' -not -path '*/exports/*' -not -path '*/coverage/*' -not -path '*/node_modules/*' -not -path '*/\.git/*' -not -path '*/\.idea/*' -print | grep -v '.DS_Store' > "./exports/$(date +%Y-%m-%d)-files-list.txt"
-find "." -maxdepth 1 -type f -name '*.md' -print -exec echo "==== Content of {} ====" \; -exec cat {} \; > "./exports/$(date +%Y-%m-%d)-root-cat.txt"
-find "." -maxdepth 1 -type f -name '*.txt' -print -exec echo "==== Content of {} ====" \; -exec cat {} \; >> "./exports/$(date +%Y-%m-%d)-root-cat.txt"
-find "." -maxdepth 1 -type f -name '*.sh' -print -exec echo "==== Content of {} ====" \; -exec cat {} \; >> "./exports/$(date +%Y-%m-%d)-root-cat.txt"
-find "./public" -type f -name '*.html' -print -exec echo "==== Content of {} ====" \; -exec cat {} \; > "./exports/$(date +%Y-%m-%d)-src-cat.txt"
-find "./public" -type f -name '*.js' -print -exec echo "==== Content of {} ====" \; -exec cat {} \; >> "./exports/$(date +%Y-%m-%d)-src-cat.txt"
+
+find '.' -maxdepth 1 -type f -name '*.md' -print -exec echo '==== Content of {} ====' \; -exec cat {} \; > "./exports/$(date +%Y-%m-%d)-root-cat.txt"
+find '.' -maxdepth 1 -type f -name '*.txt' -print -exec echo '==== Content of {} ====' \; -exec cat {} \; >> "./exports/$(date +%Y-%m-%d)-root-cat.txt"
+find '.' -maxdepth 1 -type f -name '*.sh' -print -exec echo '==== Content of {} ====' \; -exec cat {} \; >> "./exports/$(date +%Y-%m-%d)-root-cat.txt"
+
+find './.github' -type f -name '*.yml' -print -exec echo '==== Content of {} ====' \; -exec cat {} \; > "./exports/$(date +%Y-%m-%d)-ci-cat.txt"
+find './.github' -type f -name '*.js' -print -exec echo '==== Content of {} ====' \; -exec cat {} \; >> "./exports/$(date +%Y-%m-%d)-ci-cat.txt"
+
+find *'-stack' -type f -name '*.java' -not -path '*/target/*' -print -exec echo '==== Content of {} ====' \; -exec cat {} \; > "./exports/$(date +%Y-%m-%d)-stacks-cat.txt"
+find *'-stack' -type f -name '*.json' -not -path '*/target/*' -print -exec echo '==== Content of {} ====' \; -exec cat {} \; >> "./exports/$(date +%Y-%m-%d)-stacks-cat.txt"
+find *'-stack' -type f -name '*.iml' -not -path '*/target/*' -print -exec echo '==== Content of {} ====' \; -exec cat {} \; >> "./exports/$(date +%Y-%m-%d)-stacks-cat.txt"
+find *'-stack' -type f -name 'pom.xml' -not -path '*/target/*' -print -exec echo '==== Content of {} ====' \; -exec cat {} \; >> "./exports/$(date +%Y-%m-%d)-stacks-cat.txt"
+find *'-lib' -type f -name '*.java' -not -path '*/target/*' -print -exec echo '==== Content of {} ====' \; -exec cat {} \; >> "./exports/$(date +%Y-%m-%d)-stacks-cat.txt"
+find '.' -type f -name 'pom.xml' -not -path '*/target/*' -print -exec echo '==== Content of {} ====' \; -exec cat {} \; >> "./exports/$(date +%Y-%m-%d)-stacks-cat.txt"
+
+find './public' -type f -name '*.html' -print -exec echo '==== Content of {} ====' \; -exec cat {} \; > "./exports/$(date +%Y-%m-%d)-public-cat.txt"
+find './public' -type f -name '*.js' -print -exec echo '==== Content of {} ====' \; -exec cat {} \; >> "./exports/$(date +%Y-%m-%d)-public-cat.txt"
