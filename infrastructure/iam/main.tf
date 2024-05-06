8005f716374a206c943bf470bcce9f519dc13a37
Antony Cartwright <antony.cartwright@awaze.com>
2024-05-07 00:43:56 +0200

    WIP: Add Log5J runtime for Network stack

--- infrastructure/iam/main.tf
+++ infrastructure/iam/main.tf
@@ -92,7 +92,7 @@ resource "aws_iam_role_policy_attachment" "cloudtrail-attachment" { # 7
   role = aws_iam_role.iam_deploy_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSCloudTrail_FullAccess"
 }
-data "aws_iam_policy_document" "inline_policy_document" { # 8
+data "aws_iam_policy_document" "inline_policy_document" {
   statement {
     effect = "Allow"
     actions = [
@@ -114,7 +114,7 @@ resource "aws_iam_policy" "inline_policy" {
     service = var.service_name
   }
 }
-resource "aws_iam_role_policy_attachment" "deployment_role_attachment" {
+resource "aws_iam_role_policy_attachment" "deployment_role_attachment" {  # 8
   depends_on = [aws_iam_role.iam_deploy_role, aws_iam_policy.inline_policy]
   role = aws_iam_role.iam_deploy_role.name
   policy_arn = aws_iam_policy.inline_policy.arn
