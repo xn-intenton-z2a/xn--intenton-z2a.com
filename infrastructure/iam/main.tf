77ddff63c039dae8701448316ac40a51930132bd
Antony Cartwright <antony.cartwright@awaze.com>
2024-05-07 00:43:56 +0200

    WIP: Add ACM permissions

--- infrastructure/iam/main.tf
+++ infrastructure/iam/main.tf
@@ -92,7 +92,17 @@ resource "aws_iam_role_policy_attachment" "cloudtrail-attachment" { # 7
   role = aws_iam_role.iam_deploy_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSCloudTrail_FullAccess"
 }
-data "aws_iam_policy_document" "inline_policy_document" {
+resource "aws_iam_role_policy_attachment" "cloudtrail-attachment" { # 7
+  depends_on = [aws_iam_role.iam_deploy_role]
+  role = aws_iam_role.iam_deploy_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCloudTrail_FullAccess"
+}
+resource "aws_iam_role_policy_attachment" "acm-attachment" { # 8
+  depends_on = [aws_iam_role.iam_deploy_role]
+  role = aws_iam_role.iam_deploy_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonCertificateManagerFullAccess"
+}
+data "aws_iam_policy_document" "inline_policy_document" { # 9
   statement {
     effect = "Allow"
     actions = [
@@ -114,7 +124,7 @@ resource "aws_iam_policy" "inline_policy" {
     service = var.service_name
   }
 }
-resource "aws_iam_role_policy_attachment" "deployment_role_attachment" {  # 8
+resource "aws_iam_role_policy_attachment" "deployment_role_attachment" {
   depends_on = [aws_iam_role.iam_deploy_role, aws_iam_policy.inline_policy]
   role = aws_iam_role.iam_deploy_role.name
   policy_arn = aws_iam_policy.inline_policy.arn
